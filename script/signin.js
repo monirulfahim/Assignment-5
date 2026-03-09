66bd0f345f341706b8111b74ef92d1cd2627d696
monirulfahim <monirulfahim65@gmail.com>
2026-03-09 19:38:08 +0600

    update project files

--- script/signin.js
+++ script/signin.js
@@ -15,7 +15,7 @@ document.getElementById('signin-btn').addEventListener("click", function(){
 
     if(userName == 'admin' && password == 'admin123'){
         alert("Sign-in Success");
-        window.location.assign("../HTML/homepage.html")
+        window.location.assign("./HTML/homepage.html")
     }
     else{
         alert("Sign-in failed");
